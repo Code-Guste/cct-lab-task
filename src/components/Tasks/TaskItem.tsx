7578975f04d2eca463ea89c954a5d07739d18d89
unknown <auguste.uzielaite@gmail.com>
2021-11-14 17:47:34 +0200

    Edit code structure to toggle the accordion panels

--- src/components/Tasks/TaskItem.tsx
+++ src/components/Tasks/TaskItem.tsx
@@ -3,26 +3,30 @@ import classes from "./TaskItem.module.css";
 import { ReactComponent as Radio } from "../../assets/radio.svg";
 import { CSSTransition } from "react-transition-group";
 
-const TaskItem: React.FC<{ title: string; listItems: string[]; icon: any }> = (
-  props
-) => {
-  const [isActive, setIsActive] = useState(false);
-
+const TaskItem: React.FC<{
+  title: string;
+  listItems: string[];
+  icon: any;
+  onToggle: () => void;
+  active: boolean;
+}> = (props) => {
   return (
     <li
-      className={classes["items-container"]}
-      onClick={() => setIsActive(!isActive)}
+      className={`${classes["items-container"]} ${
+        props.active ? `${classes.active}` : ""
+      }`}
+      onClick={props.onToggle}
     >
       <div
         className={`${classes["task-item"]} ${
-          isActive ? `${classes.active}` : ""
+          props.active ? `${classes.active}` : ""
         }`}
       >
         <div className={classes.icon}>{props.icon} </div>
         <h3 className={classes["task-title"]}>{props.title}</h3>
       </div>
       <CSSTransition
-        in={isActive}
+        in={props.active}
         timeout={200}
         classNames={{
           enter: classes["alert-enter"],
--- src/components/Tasks/TaskItems.tsx
+++ src/components/Tasks/TaskItems.tsx
@@ -4,8 +4,17 @@ import { ReactComponent as NumberTwo } from "../../assets/number-2-icon.svg";
 import { ReactComponent as NumberFour } from "../../assets/number-4-icon.svg";
 import { ReactComponent as NumberOne } from "../../assets/number-1-icon.svg";
 import classes from "./TaskItems.module.css";
+import { useState } from "react";
 
 const TaskItems = () => {
+  const [clicked, setClicked] = useState("0");
+
+  const handleToggle = (id: string) => {
+    if (clicked === id) {
+      return setClicked("0");
+    }
+    setClicked(id);
+  };
   const taskData = [
     {
       title: "Build test task",
@@ -48,7 +57,9 @@ const TaskItems = () => {
           listItems={listItems}
           icon={icon}
           key={id}
-        ></TaskItem>
+          onToggle={() => handleToggle(id)}
+          active={clicked === id}
+        />
       ))}
     </ul>
   );
